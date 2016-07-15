9172a74303b624f13b2dd2a25843944dcdbf5d77
Gio d'Amelio <giodamelio@gmail.com>
2016-07-14 20:19:11 -0700

    Remove duplication of eslint rules

--- gulpfile.js
+++ gulpfile.js
@@ -5,51 +5,38 @@ const mocha = require('gulp-mocha');
 const SRC_FILES = 'lib/**/*.js';
 const TEST_FILES = 'test/**/*.js';
 
+const baseLinterOptions = {
+  rules: {
+    'no-console': 0,
+    indent: [2, 2],
+    quotes: [2, 'single'],
+    'linebreak-style': [2, 'unix'],
+    semi: [2, 'always'],
+    'no-undef': 2,
+  },
+  envs: ['node', 'es6'],
+  parserOptions: {
+    ecmaVersion: 6,
+    ecmaFeatures: {
+      impliedStrict: true,
+    }
+  },
+  extends: 'eslint:recommended',
+};
+
 // Linter tasks -------------------------------------------
 gulp.task('lint:src', function() {
   return gulp.src(SRC_FILES)
-    .pipe(eslint({
-      rules: {
-        'no-console': 0,
-        indent: [2, 2],
-        quotes: [2, 'single'],
-        'linebreak-style': [2, 'unix'],
-        semi: [2, 'always'],
-        'no-undef': 2,
-      },
-      envs: ['node', 'es6'],
-      parserOptions: {
-        ecmaVersion: 6,
-        ecmaFeatures: {
-          impliedStrict: true,
-        }
-      },
-      extends: 'eslint:recommended',
-    }))
+    .pipe(eslint(baseLinterOptions))
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
 });
 
 gulp.task('lint:test', function() {
   return gulp.src(TEST_FILES)
-    .pipe(eslint({
-      rules: {
-        'no-console': 0,
-        indent: [2, 2],
-        quotes: [2, 'single'],
-        'linebreak-style': [2, 'unix'],
-        semi: [2, 'always'],
-        'no-undef': 2,
-      },
+    .pipe(eslint(Object.assign(baseLinterOptions, {
       envs: ['node', 'es6', 'mocha'],
-      parserOptions: {
-        ecmaVersion: 6,
-        ecmaFeatures: {
-          impliedStrict: true,
-        }
-      },
-      extends: 'eslint:recommended',
-    }))
+    })))
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
 });
