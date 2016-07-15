53a3c43f6a09ab08c50a08ac051b7778645b6f16
Aliza Pilisuk <apilisuk@gmail.com>
2016-07-15 10:06:33 -0700

    added width and height parsing and testing

--- test/parse_test.js
+++ test/parse_test.js
@@ -6,14 +6,6 @@ const expect = require('chai').expect;
 const parse = require('../lib/parse');
 
 describe('Bitmap parsing', function() {
-  // it('test parsing', function(done) {
-  //   const image = fs.readFileSync('./img/non-palette-bitmap.bmp');
-  //   parse(image, function(image) {
-  //     expect(image).to.equal('Parsed image');
-  //     done();
-  //   });
-  // });
-
   it('testing if BM exits at beggining of file', function(done) {
     const image = new buffer.Buffer('I AM NOT AN IMAGE');
     parse(image, function(err, image) {
@@ -37,6 +29,16 @@ describe('Bitmap parsing', function() {
     parse(image, function(err, image) {
       expect(err).to.equal(null);
       expect(image.pixelOffset).to.equal(54);
+      done();
+    });
+  });
+
+  it('parse width and height', function(done) {
+    const image = fs.readFileSync('./img/non-palette-bitmap.bmp');
+    parse(image, function(err, image) {
+      expect(err).to.equal(null);
+      expect(image.width).to.equal(100);
+      expect(image.height).to.equal(100);
       done();
     });
   });
