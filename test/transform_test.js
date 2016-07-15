9f39549b7c3e1d194895c6191a93343df910e941
Gio d'Amelio <giodamelio@gmail.com>
2016-07-15 10:41:04 -0700

    Add transform file and test boilerplate

---
+++ test/transform_test.js
@@ -0,0 +1,17 @@
+const expect = require('chai').expect;
+
+const parse = require('../lib/parse');
+const transform = require('../lib/transform');
+
+describe('Bitmap transform', function() {
+  it('basic transform', function(done) {
+    const imageBuffer = fs.readFileSync('./img/non-palette-bitmap.bmp');
+    parse(imageBuffer, function(err, image) {
+      expect(err).to.equal(null);
+
+      transform(image, function() {
+        done();
+      })
+    });
+  })
+});
