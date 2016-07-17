566d96028a0cfb6f25afb5c2b6c99fcb4a332688
Gio d'Amelio <giodamelio@gmail.com>
2016-07-17 14:15:37 -0700

    Add tests for cli errors

--- bin/_bitmap_transform.js
+++ bin/_bitmap_transform.js
@@ -8,32 +8,33 @@ const cli = function(writeStream, args, callback) {
   args = args.slice(2);
 
   if (args.length < 3) {
-    callback('You must pass in a transformation type, input bitmap and output path');
+    return callback('You must pass in a transformation type, input bitmap and output path');
   }
 
   fs.readFile(args[1], function(err, imageBuffer) {
     if (err) {
-      callback('Image does not exist');
+      return callback('Image does not exist');
     }
 
     parse(imageBuffer, function(err, image) {
       if (err) {
-        callback('Unable to parse bitmap');
+        return callback('Unable to parse bitmap');
       }
       writeStream.write('Parsing bitmap...\n');
 
       transform(image, args[0], function(err, newImage) {
         if (err) {
-          callback('Error transforming image');
+          return callback('Error transforming image');
         }
         writeStream.write('Transforming...\n');
 
         write(newImage, args[2], function(err) {
           if (err) {
-            callback('Error writing transformed image');
+            return callback('Error writing transformed image');
           }
+
           writeStream.write('Image transformed...\n');
-          callback(null);
+          return callback(null);
         });
       });
     });
--- test/cli_test.js
+++ test/cli_test.js
@@ -4,7 +4,7 @@ const expect = require('chai').expect;
 const cli = require('../bin/_bitmap_transform');
 
 describe('Command line', function() {
-  it('test basic transform', function(done) {
+  it('basic transform', function(done) {
     const args = [
       null,
       null,
@@ -21,6 +21,113 @@ describe('Command line', function() {
 
     cli(testStream, args, function(err) {
       expect(err).to.equal(null);
+      expect(called).to.equal(true);
+      done();
+    });
+  });
+
+  it('invalid arguments', function(done) {
+    const args = [
+      null,
+      null
+    ];
+    let called = false;
+    const testStream = {
+      write: function() {
+        called = true;
+      }
+    };
+
+    cli(testStream, args, function(err) {
+      expect(err).to.equal('You must pass in a transformation type, input bitmap and output path');
+      expect(called).to.equal(false);
+      done();
+    });
+  });
+
+  it('invalid input image path', function(done) {
+    const args = [
+      null,
+      null,
+      'black',
+      'i-do-not-exist.bmp',
+      'output.bmp'
+    ];
+    let called = false;
+    const testStream = {
+      write: function() {
+        called = true;
+      }
+    };
+
+    cli(testStream, args, function(err) {
+      expect(err).to.equal('Image does not exist');
+      expect(called).to.equal(false);
+      done();
+    });
+  });
+
+  it('parsing error', function(done) {
+    const args = [
+      null,
+      null,
+      'black',
+      './package.json', // This is not a bitmap!
+      'output.bmp'
+    ];
+    let called = false;
+    const testStream = {
+      write: function() {
+        called = true;
+      }
+    };
+
+    cli(testStream, args, function(err) {
+      expect(err).to.equal('Unable to parse bitmap');
+      expect(called).to.equal(false);
+      done();
+    });
+  });
+
+  it('transforming error', function(done) {
+    const args = [
+      null,
+      null,
+      'this-transform-does-not-exist',
+      './img/non-palette-bitmap.bmp',
+      'output.bmp'
+    ];
+    let called = false;
+    const testStream = {
+      write: function() {
+        called = true;
+      }
+    };
+
+    cli(testStream, args, function(err) {
+      expect(err).to.equal('Error transforming image');
+      expect(called).to.equal(true);
+      done();
+    });
+  });
+
+  it('writing error', function(done) {
+    const args = [
+      null,
+      null,
+      'black',
+      './img/non-palette-bitmap.bmp',
+      ''
+    ];
+    let called = false;
+    const testStream = {
+      write: function() {
+        called = true;
+      }
+    };
+
+    cli(testStream, args, function(err) {
+      expect(err).to.equal('Error writing transformed image');
       expect(called).to.equal(true);
       done();
     });
